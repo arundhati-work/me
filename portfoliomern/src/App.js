b5721a79a24d17a5e50ef55a13409ab0b8d321c2
ArundhatiB-neu <arundhatib.work@gmail.com>
2024-12-16 14:43:48 -0500

    slight changes

--- portfoliomern/src/App.js
+++ portfoliomern/src/App.js
@@ -37,7 +37,7 @@ const ScrollManager = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="App">
         <Navbar />
         <ScrollManager />
