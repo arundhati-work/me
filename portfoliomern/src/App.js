08d55177ded731eee4e711801037e7a0598c222b
ArundhatiB-neu <arundhatib.work@gmail.com>
2024-12-17 10:07:59 -0500

    reinstalled gh-pages

--- portfoliomern/src/App.js
+++ portfoliomern/src/App.js
@@ -37,7 +37,7 @@ const ScrollManager = () => {
 
 function App() {
   return (
-    <Router basename="/portfolio">
+    <Router  basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <ScrollManager />
